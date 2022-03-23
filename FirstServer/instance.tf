108f8a95c0927b07f20c8da4aef7ed9c062e38eb
Adolfo <nusa74@gmail.com>
2022-03-23 13:58:09 -0600

    Fixed file instance.tf

--- FirstServer/instance.tf
+++ FirstServer/instance.tf
@@ -1,22 +1,25 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 3.0"
+        }
     }
-  }
 }
+
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_KEY_ID" {}
+
 provider "aws" {
-access_key = "AKIA2SQ6E3JRYDQRJWDO"
-secret_key = "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"
-region = "us-east-1"
+  region     = "us-east-2"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_KEY_ID
 }
 
-resource "aws_instance" "hello-instance" {
-  ami = "ami-0fb653ca2d3203ac1"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "hello-instance"
-  }
+resource "aws_instance" "instance-terraformcloud" {
+    ami = "ami-0fb653ca2d3203ac1"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "terraformcloud"
+    }
 }
-
